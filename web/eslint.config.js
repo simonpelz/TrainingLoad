84db7258a3de827e232e76bc91212fcb77fa1653
Simon Pelz <simon@pelz-hd.de>
2026-06-01 20:18:16 +0200

    Allow Node globals in eslint config (vite.config.ts uses process/__dirname)

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -11,7 +11,7 @@ export default tseslint.config(
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: { ...globals.browser, ...globals.node },
     },
     plugins: {
       'react-hooks': reactHooks,
